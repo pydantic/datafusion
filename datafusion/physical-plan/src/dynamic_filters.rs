bc8ae972ff41cea79a92e3edf54f4d936b149046
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2025-03-26 09:04:55 -0500

    clippy, docs

--- datafusion/physical-plan/src/dynamic_filters.rs
+++ datafusion/physical-plan/src/dynamic_filters.rs
@@ -24,6 +24,8 @@ use datafusion_physical_expr::PhysicalExpr;
 ///
 /// During query execution, operators implementing this trait can provide
 /// filter expressions that other operators can use to dynamically prune data.
+/// 
+/// See [`TopKDynamicFilterSource`] for examples.
 pub trait DynamicFilterSource: Send + Sync + std::fmt::Debug + 'static {
     /// Returns a list of filter expressions that can be used for dynamic pruning.
     fn current_filters(&self) -> Result<Vec<Arc<dyn PhysicalExpr>>>;
