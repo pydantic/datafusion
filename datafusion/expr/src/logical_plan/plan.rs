b0a044436bd27da039f7bb0422c6b89428a4877a
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2025-08-27 16:09:29 -0500

    fmt

--- datafusion/expr/src/logical_plan/plan.rs
+++ datafusion/expr/src/logical_plan/plan.rs
@@ -2656,7 +2656,10 @@ impl TableScan {
         })
     }
 
-    pub fn with_preferred_ordering(mut self, preferred_ordering: Option<Vec<SortExpr>>) -> Self {
+    pub fn with_preferred_ordering(
+        mut self,
+        preferred_ordering: Option<Vec<SortExpr>>,
+    ) -> Self {
         self.preferred_ordering = preferred_ordering;
         self
     }
